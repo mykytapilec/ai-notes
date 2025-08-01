97828ae74484cb6994cccbacfe30c8902748277f
Mikita Pilets <mikitapilets@Mikitas-MacBook-Pro.local>
2025-08-01 16:39:52 +0200

    addition of the db

---
+++ server/src/note.entity.ts
@@ -0,0 +1,13 @@
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+
+@Entity()
+export class Note {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  title: string;
+
+  @Column({ nullable: true })
+  content: string;
+}
