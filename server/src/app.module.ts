67031bb98a0b0ca6b45e488118454f7ba9d67415
Mikita Pilets <mikitapilets@Mikitas-MacBook-Pro.local>
2025-08-05 11:52:17 +0200

    env vars integration

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,22 +1,27 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
-import { Note } from './notes/note.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { NotesModule } from './notes/notes.module';
+import { Note } from './notes/note.entity';
+import { envFilePath } from './constants';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({ isGlobal: true }),
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'mikitapilets',
-      password: 'Karlson242424!',
-      database: 'ai-notes',
-      entities: [Note],
-      synchronize: true,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath,
     }),
+
+   TypeOrmModule.forRoot({
+    type: process.env.DB_TYPE as 'postgres',
+    host: process.env.DB_HOST,
+    port: +process.env.DB_PORT,
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    entities: [Note],
+    synchronize: true,
+  }),
     NotesModule,
   ],
 })
--- server/src/main.ts
+++ server/src/main.ts
@@ -1,12 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = app.get(ConfigService);
+
   app.enableCors({
     origin: '*',
   });
-  await app.listen(3000, '0.0.0.0');
-  console.log('🚀 Server started on http://localhost:3000');
+
+  const port = config.get<number>('PORT') || 3000;
+  await app.listen(port, '0.0.0.0');
+  console.log(`🚀 Server started on http://localhost:${port}`);
 }
 bootstrap();
