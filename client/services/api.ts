2b23cbdfa5b375dda97af5fccc8cd62522ba774f
Mikita Pilets <mikitapilets@Mikitas-MacBook-Pro.local>
2025-08-07 17:41:21 +0200

    baseURL conditional variable fix

--- client/services/api.ts
+++ client/services/api.ts
@@ -1,7 +1,18 @@
 import axios from 'axios';
+import Constants from 'expo-constants';
+import { Platform } from 'react-native';
 
-const api = axios.create({
-  baseURL: 'http://localhost:3000',
-});
+let baseURL = 'http://localhost:3000';
+
+if (Platform.OS !== 'web') {
+  const debuggerHost = Constants.expoConfig?.hostUri || Constants.manifest2?.extra?.hostUri;
+
+  const ip = debuggerHost?.split(':')[0];
+  if (ip) {
+    baseURL = `http://${ip}:3000`;
+  }
+}
+
+const api = axios.create({ baseURL });
 
 export default api;
